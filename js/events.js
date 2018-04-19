f271a06936c3c9c8bbe35b1486e47c7b277764b2
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-19 09:37:25 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,8 +1,12 @@
 function getIt() {
   $('p:first-child').on('click', function() {
-    alert('Hey!')
+    alert('Hey!');
     return;
   });
+}
+
+function frameIt() {
+
 }
 
 $(document).ready(function(){
