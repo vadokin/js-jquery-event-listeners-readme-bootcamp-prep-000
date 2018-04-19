47053c5d407ba19374d3ae9afbe8344003e16d3a
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-19 10:12:10 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
-//define functions here
+function getIt() {
+  $('p:first').on('click', function() {
+    alert('Hey!');
+    return;
+  });
+}
+
+function frameIt() {
+  $('img:first').on('load', function() {
+    $('img:first').addClass('tasty');
+    return;
+  });
+}
+
+function pressIt() {
+  $('input:first').on('keydown', function(key) {
+    if (key.which == 71) {
+      alert('G');
+      return;
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.");
+    return;
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
